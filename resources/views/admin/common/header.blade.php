439e206abfb162309f7f476328e027bd96b74cf4
hoangthien <hoang_thien1410@yahoo.com>
2015-09-18 20:28:05 +0700

    add token to meta header to setup ajax.

--- resources/views/admin/common/header.blade.php
+++ resources/views/admin/common/header.blade.php
@@ -4,6 +4,7 @@
 	<title>Thien Nguyen Hoang</title>
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="/ACEAdmin/assets/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="/ACEAdmin/assets/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="/ACEAdmin/assets/css/ace-fonts.css" />
